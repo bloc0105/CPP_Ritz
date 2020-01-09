56578c6f1426215fbccbb62a8c98c5807399af0a
Andy Block <adblock@mtu.edu>
2020-01-08 20:34:30 -0600

    It works!

--- src/CPP_Ritz.cpp
+++ src/CPP_Ritz.cpp
@@ -5,7 +5,7 @@
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
 //============================================================================
-#define _GLIBCXX_USE_CXX11_ABI 0
+//#define _GLIBCXX_USE_CXX11_ABI 0
 //#define _GLIBCXX_USE_CXX11_ABI 1 
 
 #include <iostream>
@@ -13,9 +13,9 @@
 #include "symengine/symbol.h"
 
 int main() {
-	std::cout << "!!!Hello World!!!" << std::endl; // prints !!!Hello World!!!
+	//std::cout << "!!!Hello World!!!" << std::endl; // prints !!!Hello World!!!
 	//const std::string x = "x";
 	const SymEngine::RCP<const SymEngine::Symbol> xx = (SymEngine::symbol("x"));
-	//std::cout << "X is " << x << std::endl;
+	std::cout << "X is " << xx << std::endl;
 	return 0;
 }
--- src/compile.sh
+++ src/compile.sh
@@ -1,3 +1,7 @@
 #g++ -I/usr/local/lib/include/symengine CPP_Ritz.cpp -o cppritz
 #g++ -lsymengine CPP_Ritz.cpp -o cppritz
-g++ -L/usr/local/lib  -lsymengine CPP_Ritz.cpp -o cppritz
+#g++ -L/usr/local/lib  -lsymengine CPP_Ritz.cpp -o cppritz
+compile_flags=`cmake --find-package -DNAME=SymEngine -DCOMPILER_ID=GNU -DLANGUAGE=CXX -DMODE=COMPILE`
+link_flags=`cmake --find-package -DNAME=SymEngine -DCOMPILER_ID=GNU -DLANGUAGE=CXX -DMODE=LINK`
+
+g++ $compile_flags CPP_Ritz.cpp $link_flags -std=c++11
